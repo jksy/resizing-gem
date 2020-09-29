dc3a8fcc3aefb66a0e7c0c34c3073c4151d5e53c
Junichiro Kasuya <junichiro.kasuya@gmail.com>
2020-09-29 17:52:46 +0900

    fix https://poke-m.airbrake.io/projects/122951/groups/2801503349558768077?tab=notice-detail

--- lib/resizing/carrier_wave/storage/file.rb
+++ lib/resizing/carrier_wave/storage/file.rb
@@ -34,12 +34,12 @@ module Resizing
           image_id = separted[:image_id]
           resp = client.delete(image_id)
           if resp['error'] == 'ActiveRecord::RecordNotFound' # 404 not found
-            model.send :write_attribute, serialization_column, nil
+            model.send :write_attribute, serialization_column, nil unless model.destroyed?
             return
           end
 
           if public_id == resp['public_id']
-            model.send :write_attribute, serialization_column, nil
+            model.send :write_attribute, serialization_column, nil unless model.destroyed?
             return
           end
 
