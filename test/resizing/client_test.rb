1e8374e77e51ae0b018aef8f08627ae0fcffb50d
Junichiro Kasuya <junichiro.kasuya@gmail.com>
2020-10-11 14:14:32 +0900

    fix tests

--- test/resizing/client_test.rb
+++ test/resizing/client_test.rb
@@ -8,8 +8,8 @@ module Resizing
       # NOP
       @configuration_template = {
         host: 'http://192.168.56.101:5000',
-        project_id: '098a2a0d-c387-4135-a071-1254d6d7e70a',
-        secret_token: '4g1cshg2lq8j93ufhvqrpjswxmtjz12yhfvq6w79jpwi7cr7nnknoqgwzkwerbs6',
+        project_id: 'e06e710d-f026-4dcf-b2c0-eab0de8bb83f',
+        secret_token: 'ewbym2r1pk49x1d2lxdbiiavnqp25j2kh00hsg3koy0ppm620x5mhlmgl3rq5ci8',
         open_timeout: 10,
         response_timeout: 20
       }
@@ -41,14 +41,14 @@ module Resizing
       VCR.use_cassette 'client/post', record: :once  do
         f = File.open('test/data/images/sample1.jpg', 'r')
         r = client.post(f, content_type: 'image/jpeg')
-        assert_equal(r['id'], 'bfdaf2b3-7ec5-41f4-9caa-d53247dd9666')
+        assert_equal(r['id'], '87263920-2081-498e-a107-9625f4fde01b')
         assert_equal(r['project_id'], Resizing.configure.project_id)
         assert_equal(r['content_type'], 'image/jpeg')
         assert(!r['latest_version_id'].nil?)
         assert(!r['latest_etag'].nil?)
         assert(!r['created_at'].nil?)
         assert(!r['updated_at'].nil?)
-        assert_equal(r['public_id'], '/projects/098a2a0d-c387-4135-a071-1254d6d7e70a/upload/images/bfdaf2b3-7ec5-41f4-9caa-d53247dd9666/vAyWaxx96gLaAzB9Bq.VbX1_pxfXJ0Jcq')
+        assert_equal(r['public_id'], '/projects/e06e710d-f026-4dcf-b2c0-eab0de8bb83f/upload/images/87263920-2081-498e-a107-9625f4fde01b/vHg9VFvdI6HRzLFbV495VdwVmHIspLRCo')
         assert_equal(r['filename'], 'sample1.jpg')
       end
     end
@@ -70,7 +70,7 @@ module Resizing
         assert(!r['updated_at'].nil?)
         assert_equal(
           r['public_id'],
-          "/projects/098a2a0d-c387-4135-a071-1254d6d7e70a/upload/images/#{name}/v6Ew3HmDAYfb3NMRdLxR45i_gXMbLlGyi"
+          "/projects/e06e710d-f026-4dcf-b2c0-eab0de8bb83f/upload/images/#{name}/vfztekhN_WoeXo8ZkCZ4i5jcQvmPpZewR"
         )
       end
     end
@@ -82,7 +82,7 @@ module Resizing
 
       client = Resizing::Client.new
       VCR.use_cassette 'client/metadata', record: :once do
-        name = 'bfdaf2b3-7ec5-41f4-9caa-d53247dd9666'
+        name = '87263920-2081-498e-a107-9625f4fde01b'
         r = client.metadata(name)
         assert_equal(r['id'], name)
         assert_equal(r['project_id'], Resizing.configure.project_id)
@@ -95,7 +95,7 @@ module Resizing
         assert(!r['width'].nil?)
         assert_equal(
           r['public_id'],
-          "/projects/098a2a0d-c387-4135-a071-1254d6d7e70a/upload/images/#{name}/v6Ew3HmDAYfb3NMRdLxR45i_gXMbLlGyi"
+          "/projects/e06e710d-f026-4dcf-b2c0-eab0de8bb83f/upload/images/87263920-2081-498e-a107-9625f4fde01b/vHg9VFvdI6HRzLFbV495VdwVmHIspLRCo"
         )
       end
     end
